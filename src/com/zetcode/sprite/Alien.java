203314aef096654b559b87e47decefedad2930b5
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-08-07 22:42:16 +0530

    Update Alien.java

--- src/com/zetcode/sprite/Alien.java
+++ src/com/zetcode/sprite/Alien.java
@@ -1,1 +1,31 @@
+package com.zetcode.sprite;
+
+import javax.swing.ImageIcon;
+
+public class Alien extends Sprite {
+
+    private Bomb bomb;
+
+    public Alien(int x, int y) {
+
+        initAlien(x, y);
+    }
+
+    private void initAlien(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+
+        bomb = new Bomb(x, y);
+
+        var alienImg = "src/images/alien.png";
+        var ii = new ImageIcon(alienImg);
+
+        setImage(ii.getImage());
+    }
+
+    public void act(int direction) {
+
+        this.x += direction;
+    }
 
