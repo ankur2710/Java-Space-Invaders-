8057de48e712359d96109faa2d3568154f080e9a
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-08-08 23:06:04 +0530

    Add player image initialization in Player class

--- src/com/zetcode/sprite/Player.java
+++ src/com/zetcode/sprite/Player.java
@@ -13,3 +13,17 @@ public class Player extends Sprite {
 
         initPlayer();
     }
+ private void initPlayer() {
+
+        var playerImg = "src/images/player.png";
+        var ii = new ImageIcon(playerImg);
+
+        width = ii.getImage().getWidth(null);
+        setImage(ii.getImage());
+
+        int START_X = 270;
+        setX(START_X);
+
+        int START_Y = 280;
+        setY(START_Y);
+    }
