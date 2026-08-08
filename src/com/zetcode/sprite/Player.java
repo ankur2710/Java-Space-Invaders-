276252faa5a44b739696fdf969c98d3281274a34
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-08-08 23:05:28 +0530

    Update Player.java

--- src/com/zetcode/sprite/Player.java
+++ src/com/zetcode/sprite/Player.java
@@ -1,1 +1,15 @@
+package com.zetcode.sprite;
 
+import com.zetcode.Commons;
+
+import javax.swing.ImageIcon;
+import java.awt.event.KeyEvent;
+
+public class Player extends Sprite {
+
+    private int width;
+
+    public Player() {
+
+        initPlayer();
+    }
