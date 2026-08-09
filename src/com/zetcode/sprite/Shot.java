2ab7a9f8da8197d4c393c4a9056878eae24c6d10
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-08-09 20:04:01 +0530

    Update Shot.java

--- src/com/zetcode/sprite/Shot.java
+++ src/com/zetcode/sprite/Shot.java
@@ -1,1 +1,13 @@
+package com.zetcode.sprite;
 
+import javax.swing.ImageIcon;
+
+public class Shot extends Sprite {
+
+    public Shot() {
+    }
+
+    public Shot(int x, int y) {
+
+        initShot(x, y);
+    }
