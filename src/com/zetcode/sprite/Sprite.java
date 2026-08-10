9678b492d809a0315afdf5d8787d00f66cdb525e
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-08-10 22:45:18 +0530

    Update Sprite.java

--- src/com/zetcode/sprite/Sprite.java
+++ src/com/zetcode/sprite/Sprite.java
@@ -1,1 +1,13 @@
+package com.zetcode.sprite;
 
+import java.awt.Image;
+
+public class Sprite {
+
+    private boolean visible;
+    private Image image;
+    private boolean dying;
+
+    int x;
+    int y;
+    int dx;
