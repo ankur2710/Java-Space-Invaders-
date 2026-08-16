3e6afb555836d6efd9b9a0f9bd9bfdceee2ef624
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-08-16 23:52:35 +0530

    Initialize player image and position in Player class

--- src/com/zetcode/sprite/Player.java
+++ src/com/zetcode/sprite/Player.java
@@ -13,3 +13,32 @@ public class Player extends Sprite {
 
         initPlayer();
     }
+ private void initPlayer() {
+
+        var playerImg = "src/images/player.png";
+        var ii = new ImageIcon(playerImg);
+
+        width = ii.getImage().getWidth(null);
+        setImage(ii.getImage());
+
+        int START_X = 270;
+        setX(START_X);
+
+        int START_Y = 280;
+        setY(START_Y);
+    }
+public void act() {
+
+        x += dx;
+
+        if (x <= 2) {
+
+            x = 2;
+        }
+
+        if (x >= Commons.BOARD_WIDTH - 2 * width) {
+
+            x = Commons.BOARD_WIDTH - 2 * width;
+        }
+    }
+  
